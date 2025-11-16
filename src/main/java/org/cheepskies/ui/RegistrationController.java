ee75667c804cf6dd62ca7c617bc5a3450a811d79
pdaenecke <pdaenecke1@student.gsu.edu>
2025-11-16 01:32:17 -0500

    Registration method

--- src/main/java/org/cheepskies/ui/RegistrationController.java
+++ src/main/java/org/cheepskies/ui/RegistrationController.java
@@ -3,6 +3,12 @@ package org.cheepskies.ui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import org.cheepskiesdb.DatabaseConnector;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class RegistrationController {
 
@@ -26,5 +32,49 @@ public class RegistrationController {
 
     @FXML
     private TextField username;
+
+    @FXML
+    void accountCreation(MouseEvent event) {
+        String first = firstName.getText();
+        String last = lastName.getText();
+        String middle = middleInitial.getText();
+        String user = username.getText();
+        String pass = password.getText();
+        String mail = email.getText();
+
+        int id = 0;
+
+        String sqlCustomers = "INSERT INTO customers (first_name, middle_initial, last_name, email) VALUES ( ?, ?, ?, ?)";
+        String sqlCredentials = "INSERT INTO credentials (customer_id, username, password) VALUES (?, ?, ?)";
+
+        try (Connection connection = DatabaseConnector.dbConnect()) {
+
+            try (PreparedStatement statement = connection.prepareStatement(sqlCustomers, PreparedStatement.RETURN_GENERATED_KEYS)) {
+                statement.setString(1, first);
+                statement.setString(2, middle);
+                statement.setString(3, last);
+                statement.setString(4, mail);
+                statement.executeUpdate();
+
+                var keys = statement.getGeneratedKeys();
+                if (keys.next()) {
+                    id = keys.getInt(1);
+                }
+            }
+
+            try (PreparedStatement statement = connection.prepareStatement(sqlCredentials)) {
+                statement.setInt(1, id);
+                statement.setString(2, user);
+                statement.setString(3, pass);
+                statement.executeUpdate();
+
+            }
+
+            System.out.print("Registration complete.");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
