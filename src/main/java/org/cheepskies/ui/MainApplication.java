881321d09715f748868de33a8d9fe99a374d6360
darrel4pf <udembadarrel@gmail.com>
2025-11-19 13:54:26 -0500

    Main page, tables, columns, and buttons set up

--- src/main/java/org/cheepskies/ui/MainApplication.java
+++ src/main/java/org/cheepskies/ui/MainApplication.java
@@ -1,4 +1,20 @@
 package org.cheepskies.ui;
-//Darrel
-public class MainApplication {
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+public class MainApplication  {
+    public static void openMain() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(RegistrationApplication.class.getResource("/org/gui/cheepskies/main-page.fxml"));
+
+        Stage stage = new Stage();
+        Scene scene = new Scene(fxmlLoader.load(), 400, 600);
+
+        stage.setTitle("Main");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
 }
--- src/main/java/org/cheepskies/ui/MainController.java
+++ src/main/java/org/cheepskies/ui/MainController.java
@@ -1,4 +1,78 @@
 package org.cheepskies.ui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 //Darrel
-public class MainController {
+public class MainController /*implements Initializable*/ {
+
+    @FXML
+    private Button add;
+
+    @FXML
+    private Button remove;
+
+    @FXML
+    private Button search;
+
+    @FXML
+    private TextField flightId;
+
+    @FXML
+    private TableColumn<Flight, String> flightIdT;
+
+    @FXML
+    private TextField price;
+
+    @FXML
+    private TableColumn<Flight, String> priceT;
+
+    @FXML
+    private TextField departureDate;
+
+    @FXML
+    private TableColumn<Flight, String> departureDateT;
+
+    @FXML
+    private TextField departureTime;
+
+    @FXML
+    private TableColumn<Flight, String> departureTimeT;
+
+    @FXML
+    private TextField arrivalTime;
+
+    @FXML
+    private TableColumn<Flight, String> arrivalTimeT;
+
+    @FXML
+    private TextField arrivalDate;
+
+    @FXML
+    private TableColumn<Flight, String> arrivalDateT;
+
+    @FXML
+    private TextField departureLocation;
+
+    @FXML
+    private TableColumn<Flight, String> departureLocationT;
+
+    @FXML
+    private TextField arrivalLocation;
+
+    @FXML
+    private TableColumn<Flight, String> arrivalLocationT;
+
+    @FXML
+    private TableView<Flight> flightsTable;
+
+    @FXML
+    private TableView<Flight> flightsTableF;
+
+    @FXML
+    private Button logout;
 }
