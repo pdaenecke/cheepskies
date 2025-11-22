4a3393160b224059679f546f9e5296370a62ce88
darrel4pf <udembadarrel@gmail.com>
2025-11-21 22:52:35 -0500

    Main page, tables, columns, and buttons set up

--- src/main/java/org/cheepskies/ui/LoginController.java
+++ src/main/java/org/cheepskies/ui/LoginController.java
@@ -61,7 +61,7 @@ public class LoginController {
     @FXML
     void registrationRedirect(MouseEvent event) throws IOException {
         try {
-            RegistrationWindow.open();
+            RegistrationApplication.openRegistration();
         } catch (IOException e) {
             System.out.print(e);
         }
